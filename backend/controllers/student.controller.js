ac05e087edac1063d87496c24c87784874c2626f
Tushar Sharma <tusharsharma2822@gmail.com>
2025-03-11 18:19:02 +0530

    update student.controller.js to improve student-related request

--- backend/controllers/student.controller.js
+++ backend/controllers/student.controller.js
@@ -22,3 +22,40 @@ export const createStudentController = async (req, res) => {
         return res.status(400).send(err.message);
     }
 }
+
+export const studentLoginController = async (req, res) => {
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const { email, password } = req.body;
+        
+        const student = await studentModel.findOne({email}).select('+password');
+
+        if(!student){
+            return res.status(400).json({
+                message: "Student Already Exists"
+            })
+        }
+
+        const isMatch = await student.isValidPassword(password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                message: "Password doesn't match!"
+            })
+        }
+
+        const token = await student.generateToken();
+
+        return res.status(200).json({student, token});
+        
+    } catch(err) {
+        return res.status(400).json(err.message);
+    }
+}
