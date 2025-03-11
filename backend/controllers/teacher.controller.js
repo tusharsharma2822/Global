58d810a832ed236e8980d973e984c4b095c3dc0a
Tushar Sharma <tusharsharma2822@gmail.com>
2025-03-11 18:37:17 +0530

    update teacher.controller.js to improve teacher related requests

--- backend/controllers/teacher.controller.js
+++ backend/controllers/teacher.controller.js
@@ -23,3 +23,40 @@ export const createTeacherController = async (req, res) => {
     }
 
 }
+
+export const teacherLoginController = async (req,res) => {
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
+
+    try {
+
+        const {email, password} = req.body;
+
+        const teacher = await teacherModel.findOne({email}).select('+password');
+
+        if(!teacher){
+            return res.status(400).json({
+                message: "Teacher Already Exist"
+            })
+        }
+
+        const isMatch = await teacher.isValidPassword(password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                message: "Password doesn't match"
+            })
+        }
+
+        const token = await teacher.generateToken();
+
+        return res.status(200).json({teacher, token});
+        
+    } catch(err) {
+        return res.status(400).send(err.message);   
+    }
+}
